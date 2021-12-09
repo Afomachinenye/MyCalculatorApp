97ea47aac61127eedab4e3a24882a726381a320c
afomachinenye <afomachukwuchinenyeonyekaba@gmail.com>
2021-12-09 21:30:27 +0100

    added some features

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+#A calculator App
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+import  math 
+def sqrt(num):
+    return math.sqrt(num)
+
+def cos_function(num):
+    return math.cos(num)
