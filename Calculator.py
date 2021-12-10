1f7f275b3f86eaa4bf807da56056408e4abd0a93
afomachinenye <afomachukwuchinenyeonyekaba@gmail.com>
2021-12-10 07:57:59 +0100

    added more features

--- Calculator.py
+++ Calculator.py
@@ -18,3 +18,37 @@ def sqrt(num):
 
 def cos_function(num):
     return math.cos(num)
+
+def sin_function(num):
+    return math.sin(num)
+
+def cos_function(num):
+    return math.tan(num)
+
+def modulus(num1, num2):
+    return num1 % num2
+
+def cube_root(num):
+    return num ** (1/3)
+
+def index(num):
+    return num ** (index)
+
+
+    print (''' Please,  select the math operation:
+    1 =  addition
+   2 = subtraction
+   3=  multiplication
+   4 = division
+   5 = square root
+   6 = cosine
+   7 = sine
+   8 = tan
+   9 = modulus
+   10 = cube root
+   11 = cube root of arrays ''')
+
+  
+    
+    
+     
--- README.py
+++ README.py
@@ -1,3 +1,7 @@
 #My Calculator App
 
+import math
+
+from Calculator import cube_root
+cube_root(9)
 
