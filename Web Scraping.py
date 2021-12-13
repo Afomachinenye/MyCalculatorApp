4d5a8f7fb299665c2fdd067db6f4f2ce3f9392aa
afomachinenye <afomachukwuchinenyeonyekaba@gmail.com>
2021-12-13 21:46:01 +0100

    changed avatar to Avatar

--- Web Scraping.py
+++ Web Scraping.py
@@ -4,6 +4,6 @@ github_user = input("Enter Github User:")
 url = "https://github.com/" + github_user
 r = requests.get(url)
 soup = bs(r.content, "html.parser")
-profile_image = soup.find("img", {"alt"  : "avatar"}) ["src"]
+profile_image = soup.find("img", {"alt"  : "Avatar"}) ["src"]
 
 print (profile_image)
